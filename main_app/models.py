ed139133ad771934e898e24ff8ffe93aa851027a
lilibella <lilibella28@hotmail.com>
2022-06-08 16:08:46 -0400

    Enter

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField
-    description = models.TextField
-    technology = models.CharField
-    link = models.CharField
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    technology = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
