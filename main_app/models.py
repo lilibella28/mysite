d918525b6004dc5620008926298c1b54c8a410d3
lilibella <lilibella28@hotmail.com>
2022-06-08 14:40:28 -0400

    Projects models and Templates updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField
+    description = models.TextField
+    technology = models.CharField
+    link = models.CharField
